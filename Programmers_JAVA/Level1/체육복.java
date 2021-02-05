ddc00f8738b19132acbc597d51dd9f15e3bf76ee
YoujeongPark <pyj0695@gmail.com>
2021-02-05 23:49:25 +0900

    20210205 체육복 vr2

--- Programmers_JAVA/Level1/체육복.java
+++ Programmers_JAVA/Level1/체육복.java
@@ -4,23 +4,37 @@ class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
         
-        int totalStudent = n; 
-        int lostNumber = lost.length; 
-        
-        Arrays<Integer> a = new Arrays<>();
-        for(int i=0 ; i < lost.length ; i++){
-            a.
+        ArrayList<Integer> lostArrayList = new ArrayList<Integer>(); 
+        for(int i=0 ; i<lost.length ; i++ ){
+            lostArrayList.add(lost[i]);
         }
         
-        for(int i=0 ; i<lost.length ; i++  )
-            for(int j=0 ; j < reserve.length ; j++ ){
-                if(Math.abs(lost[i] - reserve[j]) == 1){
-                    lostNumber -=1; 
+        ArrayList<Integer> reserveArrayList = new ArrayList<Integer>(); 
+        for(int i=0 ; i<reserve.length ; i++ ){
+            reserveArrayList.add(reserve[i]);
+        } 
+        
+        
+        for(int i=0 ; i<lostArrayList.size() ; i++  )
+            for(int j=0 ; j < reserveArrayList.size() ; j++ ){
+            if(lostArrayList.get(i) == reserveArrayList.get(j)){
+                lostArrayList.remove(lostArrayList.get(i));
+                reserveArrayList.remove(reserveArrayList.get(j));
+            }                  
+        }                
+                     
+        int lostNumber = lostArrayList.size(); 
+        
+        for(int i=0 ; i<lostArrayList.size() ; i++  )
+            for(int j=0 ; j < reserveArrayList.size() ; j++ ){
+                if(Math.abs(lostArrayList.get(i) - reserveArrayList.get(j)) == 1){
+                    lostNumber = lostNumber-1; 
+                    reserveArrayList.set(j,-1);
                     break; 
-                }                   
+                }
             }    
         
         
-        return n- lostNumber;
+        return n - lostNumber;
     }
 }
